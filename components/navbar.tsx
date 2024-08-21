cbaa8b90e4e6c8ce5cc11609188f1dbe10f6d0f9
basupatil1213 <patil.ba@northeastern.edu>
2024-08-21 02:34:30 -0400

    update navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,37 +1,36 @@
 import React from "react";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { ToggleTheme } from "./toggle-theme";
-import { NavigationMenuItem } from "@radix-ui/react-navigation-menu";
 import Image from "next/image";
 
 const NavBar = () => {
     return (
         <div>
-            <NavigationMenuItem>
+            <nav>
                 <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
                     <div className="relative flex items-center justify-between h-16">
                         <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                             <div className="flex-shrink-0 flex items-center">
                                 <Image
                                     className="block lg:hidden h-8 w-auto"
-                                    src="https://tailwindui.com/Image/logos/workflow-mark-indigo-500.svg"
+                                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
                                     alt="Workflow"
-                                    width={32}
-                                    height={32}
+                                    width={10}
+                                    height={10}
                                 />
                                 <Image
                                     className="hidden lg:block h-8 w-auto"
-                                    src="https://tailwindui.com/Image/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                                    src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
                                     alt="Workflow"
-                                    width={32}
-                                    height={32}
+                                    width={10}
+                                    height={10}
                                 />
                             </div>
                             <div className="hidden sm:block sm:ml-6">
                                 <div className="flex space-x-4">
                                     <a
                                         href="#"
-                                        className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
+                                        className="px-3 py-2 rounded-md text-sm font-medium">
                                         Dashboard
                                     </a>
                                     <SignedOut>
@@ -46,7 +45,7 @@ const NavBar = () => {
                         </div>
                     </div>
                 </div>
-            </NavigationMenuItem>
+            </nav>
         </div>
     );
 };
